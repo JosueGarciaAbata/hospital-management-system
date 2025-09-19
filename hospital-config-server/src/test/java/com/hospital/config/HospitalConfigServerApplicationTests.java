45f47fcb18c7ae6181d070578b54d881b9e80cfa
Josue Joel Garcia Abata <josuegarcab2@hotmail.com>
2025-09-19 10:19:36 -0500

    add initial config setup

---
+++ hospital-config-server/src/test/java/com/hospital/config/HospitalConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/hospital/HospitalSystemParentApplication.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSystemParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSystemParentApplication.class, args);
+	}
+
+}
