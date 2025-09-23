6c6ce6c092622d913e16f387d8ef2782fa1c12f0
DavidJosueP2 <deividjosue52@gmail.com>
2025-09-23 17:01:12 -0500

    fix(doctor): add email field

--- admin-service/src/main/java/com/hospital/admin_service/external/dto/user/CreateUserForDoctorRequest.java
+++ admin-service/src/main/java/com/hospital/admin_service/external/dto/user/CreateUserForDoctorRequest.java
@@ -1,6 +1,5 @@
 package com.hospital.admin_service.external.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -9,6 +8,7 @@ import java.util.Set;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CreateUserForDoctorRequest(
         String username,
+        String email,
         String password,
         String gender,
         String firstName,
