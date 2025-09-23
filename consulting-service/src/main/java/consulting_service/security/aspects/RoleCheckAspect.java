9d3a26be8beb4c7e05dabc2163c65f39e8a12b27
DRTX2 <davidmanjarres2004@gmail.com>
2025-09-23 13:42:31 -0500

    Fix conflict for RoleCheckAspect.java

--- consulting-service/src/main/java/consulting_service/security/aspects/RoleCheckAspect.java
+++ consulting-service/src/main/java/consulting_service/security/aspects/RoleCheckAspect.java
@@ -28,11 +28,7 @@ public class RoleCheckAspect {
         List<String> userRoles = Arrays.asList(rolesHeader.split(","));
         boolean allowed = Arrays.stream(rolesAllowed.value())
                 .anyMatch(userRoles::contains);
-<<<<<<< HEAD
-               //si se necesita al menos uno entonces cambiar por any
-=======
-               
->>>>>>> develop
+        //si se necesita al menos uno entonces cambiar por any
         if (!allowed) {
             throw new ResponseStatusException(
                     HttpStatus.FORBIDDEN,
